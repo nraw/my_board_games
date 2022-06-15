2381209a79936a4917272e5350d2748896dc60a6
Andrej-Marsic <andrej_marsic@mckinsey.com>
2022-06-15 23:17:12 +0200

    feat: add bbb

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from boardgamegeek import BGGClient
 from loguru import logger
 
+#  from my_bg.get_bbb_games import get_bbb_games
 from my_bg.get_suggested_players import get_suggested_players
 from my_bg.make_charts import make_charts
 from my_bg.settings import conf
@@ -13,6 +14,7 @@ def main():
     bgg = BGGClient()
     logger.info("Getting games")
     my_games = get_my_games(bgg)
+    #  bbb_games = get_bbb_games()
     logger.info(f"Got {len(my_games)} games.")
     game_ids = my_games.id.to_list()
     logger.info("Getting games metadata")
@@ -22,11 +24,11 @@ def main():
     suggested_players = get_suggested_players(games)
     logger.info("Got suggested players table")
     logger.info("Charting")
-    fig = make_charts(suggested_players)
+    make_charts(suggested_players)
     logger.info("Charted")
 
 
-def get_my_games(bgg):
+def get_my_games(bgg) -> pd.DataFrame:
     exclude_list = conf["exclude_list"]
     games_batch = bgg.collection("nraw", own=True, exclude_subtype="boardgameexpansion")
     games_info = {game.id: game._data for game in games_batch if "id" in dir(game)}
@@ -53,3 +55,7 @@ def shorten_name(name):
     if len(short_name) > 20:
         short_name = "".join([word[0] for word in short_name.split()])
     return short_name
+
+
+if __name__ == "__main__":
+    main()
