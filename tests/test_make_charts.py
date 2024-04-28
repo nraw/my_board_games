672834f4f9326055aaae8e40be7a9ebeea51ac28
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-04-28 21:39:40 +0200

    tests: added test for the other chart

--- tests/test_make_charts.py
+++ tests/test_make_charts.py
@@ -1,10 +1,19 @@
 from my_board_games.make_charts import *
 
 
-def tet_average_rating_chart():
+def test_average_rating_chart():
     import pandas as pd
 
     suggested_players = pd.read_csv("data/suggested_players.csv")
     fig = make_average_rating_chart(suggested_players)
     fig = fig.properties(width=1800, height=1000).configure_view(strokeWidth=0)
     fig.save("data/fig.html")
+
+
+def test_time_since_last_played_chart():
+    import pandas as pd
+
+    suggested_players = pd.read_csv("data/suggested_players.csv")
+    fig = make_time_since_last_played_chart(suggested_players)
+    fig = fig.properties(width=1800, height=1000).configure_view(strokeWidth=0)
+    fig.save("data/fig2.html")
