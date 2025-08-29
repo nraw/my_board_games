96898e39004549dc5127c35420b8b043b22b363a
Andrej-Marsic <andrej_marsic@mckinsey.com>
2025-08-29 15:02:22 +0200

    favicon

--- 11ty_site/eleventy.config.js
+++ 11ty_site/eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   
   return {
     dir: {
